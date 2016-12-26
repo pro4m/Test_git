f096ff47fe6ec58df6c0e005642b33f7f4fca51c
ADIDAPHAT <adidaphat0605@gmail.com>
2016-12-26 15:20:58 +0700

    ht 26/12/2016
    them class

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Class1
+    {
+        private int i;
+    }
+}
